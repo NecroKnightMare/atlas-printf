3cc4eb89b409067e326bc404cba877241505faa2
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-29 17:41:35 +0000

    changed header, removed prototypes

--- main.h
+++ main.h
@@ -9,11 +9,6 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-void _print_c(va_list arg)
-void _print_s(va_list arg)
-void _print_per(va_list arg)
-void _print_d(va_list arg)
-void _print_i(va_list arg)
 
 typedef struct format
 {
