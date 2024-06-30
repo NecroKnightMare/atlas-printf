2142f6269a347912836d96e5d543e128d6f6dbf0
Ariel.Lopez <alexandrea.lopez@gmail.com>
2024-06-30 19:59:41 +0000

    added info to the man page- Ariel

--- _printf.c
+++ _printf.c
@@ -12,77 +12,82 @@
  */
 int _printf(const char *format, ...)
 {
-	unsigned char *iterator;
+	unsigned char iterator;
+	*format = iterator;
 	unsigned int count = 0;/* printed character counter */
+	int d, j, dig_index, tmp, k, divide, number;
 
 	va_list args;/* holds variable args*/
 	va_start(args, format);/* initialize va_list */
 
 	/* pointer to format string */
 
-	if (!format)/* check for null format string */
+	if (!format[i])/* check for null format string */
 {
+		_putchar("Error\n", format);
 		return (-1);/* return error */
 }
 
-	while (*iterator != '\0')/* loop through format string */
+	while (format[i] != '\0')/* loop through format string */
 {
-		if (*iterator == '%')/* check for format specifier */
+		iterator++;
+		if (format[i] == '%')/* check for format specifier */
 {
 			iterator++;/* move to next character */
 
-		if (*iterator == 'c')/* check for character */
+		if (format[i] == 'c')/* check for character */
 {
 			char c = (char)va_arg(args, int);/* get character */
 			_putchar(c);/* print character */
-			count++;/* increment character counter */
+			/* increment character counter */
 }
-			else if (*iterator == 's')/* check for string */
+			else if (format[i] == 's')/* check for string */
 {
 				char *s = va_arg(args, char*); /* get string */
-				while (*s) /* loop through string */
+				while (*s != '\0') /* loop through string */
 {
-					_putchar(*s++); /* print character */
+					_putchar(*s); /* print character */
 					count++; /* increment character counter */
 }
 }
-			else if (*iterator == 'd' || *iterator == 'i') /* check for integer */
+			else if (format[i] == 'd') /* check for integer */
 {
-				int num = va_arg(args, int); /* get integer */
-				char str_num[50]; /* string to hold integer */
-				int index = 0; /* index of string */
-				int k; /* loop counter */
-				if (num < 0) /* check for negative number */
+				d = va_arg(args, int); /* get integer */
+				dig_index = 0; /* index of string -- number of int*/
+				tmp = d; /* loop counter */
+				k = dig_index;
+
+				do
 {
-					str_num[index++] = '-'; /* add negative sign */
-					num = -num; /* make number positive */
+					dig_index++;
+					tmp /= 10;
 }
-				while (num > 0) /* loop through number */
+					while (tmp > 0);
+
+				while (k >= 0) /* looping to count int*/
 {
-					str_num[index++] = '0' + (num % 10); /* add digit to string */
-					num /= 10; /* move to next digit */
+					k--;
+					divide = 1;
+
+					while (k < j)
+{
+						divide *= 10;
+						j++;
 }
-				if (index == 0) /* check for zero */
-{
-					str_num[index++] = '0'; /* add zero to string */
-}
-			str_num[index] = '\0'; /* add null terminator */
-				for (k = 0; str_num[k]; k++) /* loop through string */
-{
-					_putchar(str_num[k]); /* print character */
-					count++; /* increment character counter */
+					number = (d / divide) % 10;
+					_putchar('0' + number);
 }
 }
 			else 
 {
 				_putchar('%'); /* print % */
-				_putchar(*iterator); /* print character */
+				_putchar(format[i]); /* print character */
 				count += 2; /* increment character counter */
 }
-} 
-	else /* if not format specifier */
+}
+		else /* if not format specifier */
 {
-		_putchar(*iterator); /* just print it */
+		_putchar(format[i]); /* just print it */
 		count++; /* increment character counter */
 }
 	iterator++; /* move to next character */
