be4c605d01c20cecc6a5da5f9d2e2ba15fb9f221
internashionalist <168384665+internashionalist@users.noreply.github.com>
2024-07-01 03:36:48 -0500

    more puts work - Nash

--- _puts.c
+++ _puts.c
@@ -6,16 +6,17 @@
  *
  * Return: number of byte
  */
+ 
 int _puts(char *str)
 {
-	int count = 0;
+	int count = 0; /* count the number of bytes */
 
-	if (str)
+	if (str) /* check if str is not NULL */
 	{
-		for (count = 0; str[count] != '\0'; count++)
+		for (count = 0; str[count] != '\0'; count++) /* loop through the string */
 		{
-			_putchar(str[count]);
+			_putchar(str[count]); /* print each character */
 		}	
 	}
-	return (count);
+	return (count); /* return the number of bytes */
 }
