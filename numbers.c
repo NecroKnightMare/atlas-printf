7539ff32013d21e268c697e4822a6ba13ccc22b8
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-02 01:00:37 +0000

    no clue what happened merge conflict- fix conflict- it deleted everything... - Ariel

---
+++ numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int unsigned_int(va_list args)
+{
+        unsigned int num;
+
+        num = va_arg(args, unsigned int);
+
+        if (num == 0)
+{
+                return ( (num));
+}
+        if (num < 1)
+{
+                return (-1);
+}
+}
