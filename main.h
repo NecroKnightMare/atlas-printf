48b3096a273e6066d4e899cee0f5c5a55db2ae99
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-02 01:08:54 +0000

    commented out unused functions in header - Ariel

--- main.h
+++ main.h
@@ -4,12 +4,12 @@
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _puts(char *str);
-int unsigned_int(va_list args);
+/**int unsigned_int(va_list args);
 void _print_d(va_list args);
 void _print_p(va_list args);
 void _print_c(va_list args);
 void _print_s(va_list args);
-
+**/
 typedef struct {
         unsigned char ;
         unsigned int (*format)(va_list);
